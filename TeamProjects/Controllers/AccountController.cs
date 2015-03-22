1fa529b71cbc276fa97d8a1ab001c4b8cbb1b378
NicolaosDemosthenous <n.demosthenous-12@student.lboro.ac.uk>
2015-03-22 15:06:20 +0000

    TeamProjects

---
+++ TeamProjects/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TeamProjects.Models;
+
+
+namespace TeamProjects.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Login() 
+        {
+            IList<User> oUser = null;
+            using (team07Entities oteam07Entities = new team07Entities())
+            {
+                oUser = (from User in oteam07Entities.Users orderby User.deptCode select User).ToList(); //putting the table User into variable called oUser ---- query called linq 
+            }
+            return View(oUser);
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserDetails model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (team07Entities oteam07Entities = new team07Entities())
+                {
+                    string username = model.deptCode;
+                    string password = model.password;
+
+                    bool userValid = oteam07Entities.Users.Any(user => user.deptCode == username && user.password == password);
+
+                    if (userValid)
+                    {
+                        return RedirectToAction("MakeRequest","Request");
+                    }
+                    else
+                    {
+                        return RedirectToAction("Login","Account");
+                    }
+
+                }
+
+            }
+            return View();
+        }
+    }
+}
---
+++ TeamProjects/RequestController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TeamProjects.Models;
+
+namespace TeamProjects.Controllers
+{
+    public class RequestController : Controller
+    {
+        //
+        // GET: /Request/
+
+        public ActionResult MakeRequest()
+        {
+            return View();
+        }
+
+        public ActionResult ViewRequest()
+        {
+    
+            IList<Request> oRequestID = null;
+            using (team07Entities oteam07ViewRequest = new team07Entities())
+            {
+                oRequestID = (from Request in oteam07ViewRequest.Requests orderby Request.requestID select Request).ToList(); //putting the table User into variable called oUser ---- query called linq 
+            }
+            return View(oRequestID);
+      
+        }
+
+    }
+}
